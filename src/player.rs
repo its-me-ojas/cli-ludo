4a73b9e6e6eced058d263e82f4261dca565879ef
its-me-ojas <crestfallenwizard@gmail.com>
2023-11-13 03:40:23 +0530

    player implemented

---
+++ src/player.rs
@@ -0,0 +1,37 @@
+struct Player {
+    name: String,
+    color: String,
+    tokens: Vec<Token>,
+}
+
+struct Token {
+    id: usize,
+    position: usize,
+}
+
+impl Player {
+    // constructor
+    fn new(name: &str, color: &str) -> Player {
+        Player {
+            name: String::from(name),
+            color: String::from(color),
+            tokens: vec![Token::new(1), Token::new(2), Token::new(3), Token::new(4)],
+        }
+    }
+
+    fn display_info(&self) {
+        println!("Player: {}", self.name);
+        println!("Color: {}", self.color);
+        println!("Tokens: ");
+        for token in &self.tokens {
+            println!("Token {} : Position {}", token.id, token.position);
+        }
+    }
+}
+
+impl Token {
+    // constructor
+    fn new(id: usize) -> Token {
+        Token { id, position: 0 }
+    }
+}
